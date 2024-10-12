781da96529c97b9a2ad3d648df37b7a20f2c055a
regularpoe <vedranvinko@protonmail.com>
2024-10-12 06:01:34 +0200

    Add logging

--- cookiejar/app.rb
+++ cookiejar/app.rb
@@ -1,38 +1,84 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'logger'
 require 'sinatra/base'
 
+class MultiIO
+  def initialize(*targets)
+    @targets = targets
+  end
+
+  def write(*args)
+    @targets.each { |t| t.write(*args) }
+  end
+
+  def close
+    @targets.each(&:close)
+  end
+
+  def flush
+    @targets.each(&:flush)
+  end
+end
+
 class App < Sinatra::Base
+  configure do
+    log_file = File.new("#{settings.root}/app.log", 'a+')
+    log_file.sync = true
+
+    multi_logger = Logger.new(MultiIO.new(STDOUT, log_file))
+
+    multi_logger.level = Logger::INFO
+
+    use Rack::CommonLogger, multi_logger
+
+    set :logger, multi_logger
+  end
+
   before do
     content_type :json
     logger.info "Headers: #{request.env.select { |k, v| k.start_with?('HTTP_') }}"
+    logger.info "Request: #{request.request_method} #{request.path_info} with params #{params}"
+  end
+
+  after do
+    logger.info "Response: #{response.status}"
   end
 
   get '/' do
-    { status: 200, result: 'Hello from cookiejar' }.to_json
+    result = { status: 200, result: 'Hello from cookiejar' }.to_json
+    logger.info "GET / response: #{result}"
+    result
   end
 
   get '/slow' do
-    sleep(rand(1..5))
+    delay = rand(1..5)
+    logger.info "Sleeping for #{delay} seconds"
+    sleep(delay)
 
-    { status: 200, result: 'Hello from GET /slow' }.to_json
+    result = { status: 200, result: 'Hello from GET /slow' }.to_json
+    logger.info "GET /slow response: #{result}"
+    result
   end
 
   get '/random_error' do
     if rand < 0.2
+      logger.error "Random error occurred!"
       status 500
       "Oops! Something went wrong!"
     else
-      { status: 200, result: 'Hello from GET /random_error' }.to_json
+      result = { status: 200, result: 'Hello from GET /random_error' }.to_json
+      logger.info "GET /random_error response: #{result}"
+      result
     end
   end
 
   get '/stream' do
     content_type 'text/plain'
+    logger.info "Streaming text in chunks..."
 
-    text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at odio eu lacus fringilla vestibulum. Nulla nec augue sapien. Quisque venenatis nibh vel quam laoreet tincidunt. Sed sagittis rutrum urna, sit amet pretium est gravida vel. In ullamcorper elit elit, eget malesuada quam hendrerit nec. Donec venenatis tellus nec mauris semper vehicula. Sed bibendum tellus a mauris vehicula consectetur. Praesent vel tellus vel odio semper aliquam. Vestibulum auctor enim ac elit sagittis, ac euismod odio consequat. Nulla faucibus quam eget risus efficitur luctus. Sed vel justo quis sapien vestibulum malesuada. Ut bibendum ante ac lacinia pellentesque. Donec lobortis enim nunc, id consequat est vestibulum nec.'
-
+    text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...'
     chunk_size = 10
     delay = 0.1
 
@@ -42,7 +88,6 @@ class App < Sinatra::Base
         out << chunk.join
       end
     end
-
   end
 
   get '/status' do
@@ -54,7 +99,9 @@ class App < Sinatra::Base
       current_time: current_time
     }
 
-    response.to_json
+    result = response.to_json
+    logger.info "GET /status response: #{result}"
+    result
   end
-
 end
+
